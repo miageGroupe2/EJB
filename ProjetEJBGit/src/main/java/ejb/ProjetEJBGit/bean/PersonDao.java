2fa80cbdc4316400e0f84dda5b0d7cdc7f8b7155
ludovic <fort0192@etudiant.univ-nancy2.fr>
2012-01-22 14:26:54 +0100

    etht

---
+++ ProjetEJBGit/src/main/java/ejb/ProjetEJBGit/bean/PersonDao.java
@@ -0,0 +1,34 @@
+package ejb.ProjetEJBGit.bean;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import ejb.ProjetEJBGit.qualifier.DataRepository;
+import ejb.ProjetEJBGit.model.Person;
+
+/**
+ * Simple PersonDao bean that handles persisting a person entity and returning a
+ * list of people in the database. Implemented as an EJB for the transaction
+ * facilities. 
+ * 
+ */
+@Stateless
+public class PersonDao {
+
+	@Inject
+	@DataRepository
+	private EntityManager entityManager;
+
+	public void savePerson(Person p) {
+		entityManager.persist(p);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Person> getPeople() {
+		return entityManager.createQuery("select p from Person p")
+				.getResultList();
+	}
+}
